28053b8a112b5684ba16686e92c28e831c071521
thundermo <113253460+thundermo@users.noreply.github.com>
2023-04-04 18:47:53 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -12,93 +12,85 @@ function showDetail(id) {
 
 }
 
-
 //read data.json
-fetch('data.json')
-  .then(response => response.json())
-  .then(tracksData => {
-    console.log(tracksData);
-        // Use the data as an object
-            
-    /*  the append elements in html code:
-        <div class="w3-third w3-container">
-        <div id="'track' + trackListIndex" class="w3-container w3-margin-bottom w3-white">
-            <p><b> <button>˅ </button> Ap Lei Chau Wind Tower Park </p> 
-            <div id="'trackDetail'+trackListIndex" style="display:none;">
-            <p>Total Track Length: 1200 m<br>Calories consumed: 50-60 Cal</p>
-            <p>MTR: Lei Tung Station Exit A1<br>Bus: 90, 90B, 90C, 91, 91A, 93, 93C, 95C, 95P, 95, 99, 171, 590, 592, 595, 671<br>Public Light Bus: 27, 27A, 29, 29A, 36, 36X, 37, 37A, 63</p>
-            <img src="https://www.lcsd.gov.hk/en/sportforall/common/graphics/en/walk/map_01.jpg" style="width:100%">
-            </div>
-        </div>
-        </div>
-    */
-    //append track list block to html
-    tracksData.forEach((track,trackListIndex) => {  
-    let div = document.createElement('div');
-    div.setAttribute('class' ,'w3-rest w3-container trackDiv');
-    div.setAttribute('id', 'track' + trackListIndex);
-    
+const request = new XMLHttpRequest();
+request.open('GET', 'data.json', false); 
+request.send(null);
 
-    let trackDiv = document.createElement('div');
-    trackDiv.setAttribute('class' ,'w3-container w3-margin-bottom w3-sand');  
-    
-    let TrackDetailDiv = document.createElement('div');
-    TrackDetailDiv.setAttribute('id', 'trackDetail' + trackListIndex);
-    TrackDetailDiv.setAttribute('style', "display:none;");
-    
-    let showDetailArrow = document.createElement('button');
-    showDetailArrow.innerHTML = '˅ '; 
-    showDetailArrow.setAttribute('id', 'showDetailArrow' + trackListIndex);
-    showDetailArrow.setAttribute('class', 'text-like-btn');
-    
-    let pTrackTile = document.createElement('p');
-    pTrackTile.setAttribute('herf', '#track' + trackListIndex);
-    pTrackTile.addEventListener('click', function(){
-        showDetail('trackDetail'+trackListIndex);
-        if  (document.getElementById('showDetailArrow' + trackListIndex).innerHTML == '˅ '){
-        document.getElementById('showDetailArrow' + trackListIndex).innerHTML = '˄ ';
-        document.getElementById('track' + trackListIndex).scrollIntoView({ behavior: 'smooth' });
-        }else{
-            document.getElementById('showDetailArrow' + trackListIndex).innerHTML = '˅ ';
-        };
-    });
+if (request.status === 200) {
+  var tracksData = JSON.parse(request.responseText);
+  console.log(tracksData);
+} else {
+  console.error('Error:', request.status);
+}
 
-    let bTrackTitle = document.createElement('b');
-    
-    let pTrack = document.createElement('p');
-    pTrack.innerHTML = track.Route_en;
-    
-    let pTrackDetai = document.createElement('p');
-    pTrackDetai.innerHTML = track.HowToAccess_en;
+tracksData.forEach((track,trackListIndex) => {  
+let div = document.createElement('div');
+div.setAttribute('class' ,'w3-rest w3-container trackDiv');
+div.setAttribute('id', 'track' + trackListIndex);
 
-    let img = document.createElement('img');
-    img.src = track.MapURL_en;
-    img.style.width = '100%';
-    bTrackTitle.appendChild(showDetailArrow);
-    bTrackTitle.appendChild(document.createTextNode(track.Title_en));
-    pTrackTile.appendChild(bTrackTitle);
-    trackDiv.appendChild(pTrackTile);
-    TrackDetailDiv.appendChild(pTrack);
-    TrackDetailDiv.appendChild(pTrackDetai);
-    TrackDetailDiv.appendChild(img);  
-    trackDiv.appendChild(TrackDetailDiv);
-    div.appendChild(trackDiv)
-    document.getElementById('trackList').appendChild(div); 
-    });
 
-    function selectDistrict(selectedDistrict){
-        if(selectedDistrict != 'all'){
-            tracksData.District_en.forEach((trackDistrict,i) => { 
-                if (trackDistrict != selectedDistrict){
-                    document.getElementById('track'+i).setAttribute('style','display:none;');
-                }
-            });
-        }else{
-            document.querySelectorAll('.trackDiv').forEach(div => {
-                div.style.display = 'none';
-            });
-        }
-    }
+let trackDiv = document.createElement('div');
+trackDiv.setAttribute('class' ,'w3-container w3-margin-bottom w3-sand');  
 
-})
-.catch(error => console.error(error));  
+let TrackDetailDiv = document.createElement('div');
+TrackDetailDiv.setAttribute('id', 'trackDetail' + trackListIndex);
+TrackDetailDiv.setAttribute('style', "display:none;");
+
+let showDetailArrow = document.createElement('button');
+showDetailArrow.innerHTML = '˅ '; 
+showDetailArrow.setAttribute('id', 'showDetailArrow' + trackListIndex);
+showDetailArrow.setAttribute('class', 'text-like-btn');
+
+let pTrackTile = document.createElement('p');
+pTrackTile.setAttribute('herf', '#track' + trackListIndex);
+pTrackTile.addEventListener('click', function(){
+    showDetail('trackDetail'+trackListIndex);
+    if  (document.getElementById('showDetailArrow' + trackListIndex).innerHTML == '˅ '){
+    document.getElementById('showDetailArrow' + trackListIndex).innerHTML = '˄ ';
+    document.getElementById('track' + trackListIndex).scrollIntoView({ behavior: 'smooth' });
+    }else{
+        document.getElementById('showDetailArrow' + trackListIndex).innerHTML = '˅ ';
+    };
+});
+
+let bTrackTitle = document.createElement('b');
+
+let pTrack = document.createElement('p');
+pTrack.innerHTML = track.Route_en;
+
+let pTrackDetai = document.createElement('p');
+pTrackDetai.innerHTML = track.HowToAccess_en;
+
+let img = document.createElement('img');
+img.src = track.MapURL_en;
+img.style.width = '100%';
+bTrackTitle.appendChild(showDetailArrow);
+bTrackTitle.appendChild(document.createTextNode(track.Title_en));
+pTrackTile.appendChild(bTrackTitle);
+trackDiv.appendChild(pTrackTile);
+TrackDetailDiv.appendChild(pTrack);
+TrackDetailDiv.appendChild(pTrackDetai);
+TrackDetailDiv.appendChild(img);  
+trackDiv.appendChild(TrackDetailDiv);
+div.appendChild(trackDiv)
+document.getElementById('trackList').appendChild(div); 
+});
+
+
+  
+function selectDistrict(selectedDistrict){
+    if (selectedDistrict != 'ALL'){
+        tracksData.forEach((track,i) => {            
+            if (track.District_en != selectedDistrict){
+                document.getElementById('track'+i).setAttribute('style','display:none;');
+            }else{
+                document.getElementById('track'+i).setAttribute('style','display:block;');
+            }
+        });
+    }else{
+        tracksData.forEach((track,i) => {
+            document.getElementById('track'+i).setAttribute('style','display:block;');
+        })
+    }          
+}
